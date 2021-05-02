8fdf0ae5dde45c6c7e9e02fb413bee5c67debb36
Milan Magyar <milan.magyar@gmail.com>
2021-05-02 15:31:03 +0200

    Use preact on options page

--- src/options.ts
+++ src/options.ts
@@ -1,32 +1,74 @@
 import { SERVICES } from './constants';
 import { getTranslations } from './translations';
 
-const deleteButtonClickHandler = (e) => {
-  chrome.runtime.sendMessage({
-    message: SERVICES.CLEAR_NAMESPACE,
-    namespace: e.currentTarget.getAttribute('data-namespace')
-  }, (response) => {
-    renderStorageInfo();
+const { html, render, useEffect, useState } = window['htmPreact'];
+
+const translations = getTranslations({
+  en: {
+    storageTitle: 'Storage',
+    storageDomainLabel: 'Domain',
+    storageSizeLabel: 'Size',
+    deleteButton: 'Delete'
+  },
+  hu: {
+    storageTitle: 'Tárolt adatok',
+    storageDomainLabel: 'Domain',
+    storageSizeLabel: 'Méret',
+    deleteButton: 'Törlés'
+  }
+});
+
+const getNamespacesInfo = () => {
+  return new Promise((resolve) => {
+    chrome.runtime.sendMessage({
+      message: SERVICES.GET_NAMESPACES_INFO
+    }, (response) => {
+      resolve(response.namespaces);
+    });
   });
 };
 
-const renderStorageInfo = () => {
-  const translations = getTranslations({
-    en: {
-      storageTitle: 'Storage',
-      storageDomainLabel: 'Domain',
-      storageSizeLabel: 'Size',
-      deleteButton: 'Delete'
-    },
-    hu: {
-      storageTitle: 'Tárolt adatok',
-      storageDomainLabel: 'Domain',
-      storageSizeLabel: 'Méret',
-      deleteButton: 'Törlés'
-    }
+const clearNamespace = (namespace) => {
+  return new Promise((resolve) => {
+    chrome.runtime.sendMessage({
+      message: SERVICES.CLEAR_NAMESPACE,
+      namespace: namespace
+    }, () => {
+      resolve();
+    });
   });
+};
 
-  document.getElementsByClassName('options-block')[0].innerHTML = `
+const faviconMap = {
+  'hasznaltauto.hu': 'https://www.hasznaltauto.hu/favicon.ico',
+  'ingatlan.jofogas.hu': 'https://ingatlan.jofogas.hu/img/favicon.ico',
+  'ingatlan.com': 'https://ingatlan.com/images/favicons/favicon.ico',
+  'mobile.de': 'https://suchen.mobile.de/favicon.ico',
+  'immobilienscout24.de': 'https://www.immobilienscout24.de/favicon.ico'
+};
+
+const Options = () => {
+  const [namespaces, setNamespaces] = useState([]);
+
+  const getNamespacesInfoAsync = async () => {
+    setNamespaces(await getNamespacesInfo());
+  };
+
+  const clearNamespaceAsync = async (namespace) => {
+    await clearNamespace(namespace);
+
+    await getNamespacesInfoAsync();
+  };
+
+  useEffect(() => {
+    getNamespacesInfoAsync();
+  }, []);
+
+  const deleteButtonClickHandler = (e) => {
+    clearNamespaceAsync(e.currentTarget.getAttribute('data-namespace'));
+  };
+
+  return html`
     <h4>${translations.storageTitle}</h4>
     <table id="storage-info">
       <thead>
@@ -34,37 +76,17 @@ const renderStorageInfo = () => {
       <th>${translations.storageSizeLabel}</th>
       <th></th>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        ${namespaces.map(namespace => html`
+          <tr>
+            <td><img src="${faviconMap[namespace.name] || ''}" width="16" height="16" style="margin: 0 3px -3px 0;" /> ${namespace.name}</td>
+            <td>${namespace.megaBytesInUse} MB</td>
+            <td><button data-namespace="${namespace.name}" onClick=${deleteButtonClickHandler}>${translations.deleteButton}</button></td>
+          </tr>
+        `)}
+      </tbody>
     </table>
   `;
-
-  chrome.runtime.sendMessage({
-    message: SERVICES.GET_NAMESPACES_INFO
-  }, (response) => {
-    const storageInfoContainer = document.getElementById('storage-info').getElementsByTagName('tbody')[0];
-
-    const faviconMap = {
-      'hasznaltauto.hu': 'https://www.hasznaltauto.hu/favicon.ico',
-      'ingatlan.jofogas.hu': 'https://ingatlan.jofogas.hu/img/favicon.ico',
-      'ingatlan.com': 'https://ingatlan.com/images/favicons/favicon.ico',
-      'mobile.de': 'https://suchen.mobile.de/favicon.ico',
-      'immobilienscout24.de': 'https://www.immobilienscout24.de/favicon.ico'
-    };
-
-    storageInfoContainer.innerHTML = response.namespaces.map(namespace => `
-      <tr>
-        <td><img src="${faviconMap[namespace.name] || ''}" width="16" height="16" style="margin: 0 3px -3px 0;" /> ${namespace.name}</td>
-        <td>${namespace.megaBytesInUse} MB</td>
-        <td><button data-namespace="${namespace.name}">${translations.deleteButton}</button></td>
-      </tr>
-    `).join('');
-
-    const buttons = storageInfoContainer.getElementsByTagName('button');
-
-    for (let i = 0; i< buttons.length; i++) {
-      buttons[i].addEventListener('click', deleteButtonClickHandler);
-    }
-  });
 };
 
-renderStorageInfo();
+render(html`<${Options} />`, document.getElementById('root'));
